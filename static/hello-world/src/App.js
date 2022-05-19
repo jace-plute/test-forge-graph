038caf4ad7ec133416a9f1b0c8c1428947a31616
Indy Plute <jaceplute@gmail.com>
2022-05-19 16:24:24 -0500

    Test graph for demo.

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -58,13 +58,41 @@ Chart.register(
 function thing() {
   const ctx = document.getElementById("myChart");
   const myChart = new Chart(ctx, {
-    type: "bar",
+    type: "line",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: [
+        "Sprint 1",
+        "Sprint 2",
+        "Sprint 3",
+        "Sprint 4",
+        "Sprint 5",
+        "Sprint 6",
+      ],
       datasets: [
         {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
+          label: "Story Points for Team Wade",
+          data: [30, 27, 32, 20, 25, 31],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+        {
+          label: "Story Points Baseline",
+          data: [28, 28, 28, 28, 28, 28],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -104,8 +132,10 @@ function App() {
 
   return (
     <div>
-      <canvas id="myChart" width="400" height="400"></canvas>
-      {thing()}
+      <div style={{ maxHeight: "500px", maxWidth: "500px" }}>
+        <canvas id="myChart" width="100" height="100"></canvas>
+        {thing()}
+      </div>
     </div>
   );
 }
